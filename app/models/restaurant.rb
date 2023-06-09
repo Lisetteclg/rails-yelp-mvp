b1a07fe3b6914d5bb3acc13dc475e4156b4ebd1d
Lisette Gonzalez <lisetteclg.g@gmail.com>
2023-06-08 20:42:29 -0500

    restaurant reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+  has_many :reviews, dependent: :destroy
+
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :name, :address, :category, presence: true
 end
